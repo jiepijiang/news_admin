433ba2797e8ba863b016ef8505e2c46fa6db643b
jiepijiang <1069841961@qq.com>
2020-04-11 10:23:48 +0800

    安装element ui组件库,并且在mainjs中全局配置和注册,并且在APPvue中测试是否安装成功

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <el-button type="primary">主要按钮</el-button>
+  </div>
 </template>
 
 <style lang="less">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入 element ui 组件库的配置 -- 全局导入(可以在官网选择按需导入)
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 注册element ui 组件 -- 注册全局
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
